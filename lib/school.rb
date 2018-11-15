aa7167f59abd34a29952f2806bd686e0ce333752
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-11-15 04:28:09 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,10 +21,15 @@ class School
   end 
   
   def sort 
+<<<<<<< HEAD
     @roster.sort.each do |key, value|
      @roster[key] = value.sort 
     end
     @roster
+=======
+    @roster.sort do |key, value|
+    end 
+>>>>>>> d86714e227d644e2b5580d45498f215bd8e361a3
   end 
   
 end 
